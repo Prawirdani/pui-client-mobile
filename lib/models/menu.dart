3b75b0fc34c2bf81cfed59b73efe47e5f12d0d43
prawirdani <prawirdani@gmail.com>
2024-05-25 01:15:13 +0700

    add menu & kategori json factory

--- lib/models/menu.dart
+++ lib/models/menu.dart
@@ -1,3 +1,10 @@
+import 'package:flutter_application_4/util/http_client.dart';
+
+String _formatUrl(String imageName) {
+  final url = Uri.https(baseURL, "/api/images/$imageName");
+  return url.toString();
+}
+
 class Menu {
   final int id;
   final String nama;
@@ -6,11 +13,33 @@ class Menu {
   final int harga;
   final String url;
 
-  Menu(this.id, this.nama, this.deskripsi, this.harga, this.url, this.kategori);
+  Menu(
+      {required this.nama,
+      required this.deskripsi,
+      required this.kategori,
+      required this.harga,
+      required this.url,
+      required this.id});
+
+  factory Menu.fromJSON(Map<String, dynamic> json) {
+    final kategori = Kategori.fromJSON(json);
+    return Menu(
+      id: json['id'],
+      nama: json['nama'],
+      deskripsi: json['deskripsi'],
+      kategori: kategori,
+      harga: json['harga'],
+      url: _formatUrl(json['url']),
+    );
+  }
 }
 
 class Kategori {
   final int id;
   final String nama;
-  Kategori(this.id, this.nama);
+  Kategori({required this.id, required this.nama});
+
+  factory Kategori.fromJSON(Map<String, dynamic> json) {
+    return Kategori(id: json['id'], nama: json['nama']);
+  }
 }
