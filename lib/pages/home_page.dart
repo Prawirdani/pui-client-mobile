845e43c464d12f3be0b82985ebb80716213f7b18
prawirdani <prawirdani@gmail.com>
2024-05-25 01:15:57 +0700

    add & register menu provider

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_4/models/meja.dart';
 import 'package:flutter_application_4/providers/meja_provider.dart';
+import 'package:flutter_application_4/providers/menu_provider.dart';
 import 'package:flutter_application_4/widgets/meja_card.dart';
 import 'package:provider/provider.dart';
 
@@ -15,11 +16,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final mejaProvider = Provider.of<MejaProvider>(context);
+    final menuProvider = Provider.of<MenuProvider>(context);
+
+    fetchData() async {
+      await Future.wait([mejaProvider.fetch(), menuProvider.fetch()]);
+    }
+
     return Column(
       children: [
         mejaProvider.daftarMeja.isEmpty
             ? FutureBuilder(
-                future: mejaProvider.fetch(),
+                future: fetchData(),
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return const Expanded(
---
+++ lib/providers/menu_provider.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_4/models/menu.dart';
+import 'package:flutter_application_4/util/http_client.dart';
+import 'package:http/http.dart' as http;
+
+class MenuProvider extends ChangeNotifier {
+  late String _token;
+  List<Menu> _listMenu = [];
+
+  List<Menu> get listMenu => _listMenu;
+
+  registerToken(String token) {
+    _token = token;
+  }
+
+  Future<void> fetch() async {
+    debugPrint("fetchin Menus");
+    _listMenu = [];
+    try {
+      final url = Uri.https(baseURL, "/api/v1/menus");
+      final res =
+          await http.get(url, headers: {"Authorization": "Bearer $_token"});
+      if (res.statusCode == 200) {
+        final data = jsonDecode(res.body)['data'];
+        debugPrint(data.toString());
+        for (var each in data) {
+          var menu = Menu.fromJSON(each);
+          _listMenu.add(menu);
+        }
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+}
